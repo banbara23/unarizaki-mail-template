cd1284c7026cd4152dc2076bb82533fe92570c97
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2018-12-28 14:29:15 +0900

    右側のメニュー開発中、一旦コミット

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <v-app dark>
+  <v-app>
+
+    <!-- 右ドロワー -->
     <v-navigation-drawer
       v-model="drawerRight"
       fixed
@@ -7,17 +9,57 @@
       clipped
       app
     >
-      <v-list dense>
-        <v-list-tile @click.stop="right = !right">
-          <v-list-tile-action>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+      <div class="container fluid pb-0">
+        <!-- <v-subheader class="black--text title">入力</v-subheader> -->
+        <p>入力</p>
+        <div>
+          <v-text-field
+            label="担当者名"
+            placeholder=""
+          ></v-text-field>
+        </div>
+
+        <div>
+          <v-text-field
+            label="名前"
+            placeholder=""
+          ></v-text-field>
+        </div>
+
+        <div>
+          <v-text-field
+            label="チェックイン"
+            placeholder=""
+          ></v-text-field>
+        </div>
+
+        <div>
+          <v-text-field
+            label="チェックアウト"
+            placeholder=""
+          ></v-text-field>
+        </div>
+
+        <div>
+          <v-select
+            :items="items"
+            box
+            label="ご宿泊先"
+          ></v-select>
+        </div>
+
+        <div>
+          <v-select
+            :items="items"
+            box
+            label="お部屋タイプ"
+          ></v-select>
+        </div>
+
+      </div>
     </v-navigation-drawer>
+
+    <!-- ツールバー -->
     <v-toolbar
       color="blue-grey"
       dark
@@ -30,72 +72,52 @@
       <v-spacer></v-spacer>
       <v-toolbar-side-icon @click.stop="drawerRight = !drawerRight"></v-toolbar-side-icon>
     </v-toolbar>
+
+    <!-- 左ナビゲーション -->
     <v-navigation-drawer
       v-model="drawer"
       fixed
       app
     >
-      <v-list dense>
-        <v-list-tile @click.stop="left = !left">
-          <v-list-tile-action>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+      <v-subheader>テンプレート</v-subheader>
+      <v-list expand="true">
+        <v-list-group
+          v-for="item in items"
+          v-model="item.active"
+          :key="item.title"
+          :prepend-icon="item.action"
+          no-action
+        >
+          <v-list-tile slot="activator">
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile
+            v-for="subItem in item.items"
+            :key="subItem.title"
+            @click=""
+          >
+            <v-list-tile-content>
+              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-icon>{{ subItem.action }}</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
-    <v-navigation-drawer
+
+    <!-- <v-navigation-drawer
       v-model="left"
       temporary
       fixed
-    ></v-navigation-drawer>
-    <v-content>
-      <v-container
-        fluid
-        fill-height
-      >
-        <v-layout
-          justify-center
-          align-center
-        >
-          <v-flex shrink>
-            <v-tooltip right>
-              <v-btn
-                slot="activator"
-                :href="source"
-                icon
-                large
-                target="_blank"
-              >
-                <v-icon large>code</v-icon>
-              </v-btn>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <v-btn
-                slot="activator"
-                icon
-                large
-                href="https://codepen.io/johnjleider/pen/KQrPKJ"
-                target="_blank"
-              >
-                <v-icon large>mdi-codepen</v-icon>
-              </v-btn>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      v-model="right"
-      right
-      temporary
-      fixed
-    ></v-navigation-drawer>
+    ></v-navigation-drawer> -->
 
+    <!-- コンテンツ -->
     <v-content>
       <v-container>
         <nuxt />
@@ -110,12 +132,63 @@ export default {
   data: () => ({
     drawer: null,
     drawerRight: null,
-    right: false,
-    left: false,
-    title: 'うなりざきメールテンプレ'
-  }),
-  props: {
-    source: String
-  }
+    // right: false,
+    // left: false,
+    title: 'うなりざきメールテンプレ',
+    items: [
+      {
+        action: 'local_activity',
+        title: '2本目〜',
+        active: true,
+        items: [
+          { title: 'PM～他館' },
+          { title: 'PM～宿泊有' },
+          { title: 'PM～徒歩圏内宿' }
+        ]
+      },
+      {
+        action: 'restaurant',
+        title: '3本目〜',
+        active: true,
+        items: [
+          { title: 'PM～他館' },
+          { title: 'PM～宿泊有' },
+          { title: 'PM～徒歩圏内宿' }
+        ]
+      },
+      {
+        action: 'school',
+        title: 'Education',
+        active: true,
+        items: [{ title: 'List Item' }]
+      },
+      {
+        action: 'directions_run',
+        title: 'Family',
+        active: true,
+        items: [{ title: 'List Item' }]
+      },
+      {
+        action: 'healing',
+        title: 'Health',
+        active: true,
+        items: [{ title: 'List Item' }]
+      },
+      {
+        action: 'content_cut',
+        title: 'Office',
+        active: true,
+        items: [{ title: 'List Item' }]
+      },
+      {
+        action: 'local_offer',
+        title: 'Promotions',
+        active: true,
+        items: [{ title: 'List Item' }]
+      }
+    ],
+    valid: false
+    // nameRules: [v => !!v || 'required']
+  })
 }
 </script>
