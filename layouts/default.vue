20be911a1ebbc201dca5f06461b87d1fe47716df
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2018-12-30 23:21:09 +0900

    default.vueの左右メニューをindex.vueへ移動

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,111 +1,5 @@
 <template>
   <v-app>
-
-    <!-- 右ドロワー -->
-    <v-navigation-drawer
-      v-model="drawerRight"
-      fixed
-      right
-      clipped
-      app
-    >
-      <div class="container fluid pb-0">
-        <!-- <v-subheader class="black--text title">入力</v-subheader> -->
-        <p>入力</p>
-        <div>
-          <v-text-field
-            label="担当者名"
-            placeholder=""
-          ></v-text-field>
-        </div>
-
-        <div>
-          <v-text-field
-            label="名前"
-            placeholder=""
-          ></v-text-field>
-        </div>
-
-        <!-- <div>
-          <v-text-field
-            label="チェックイン"
-            placeholder=""
-          ></v-text-field>
-        </div> -->
-        <div>
-          <v-menu
-            :close-on-content-click="false"
-            v-model="menu2"
-            :nudge-right="40"
-            lazy
-            transition="scale-transition"
-            offset-y
-            full-width
-            min-width="290px"
-          >
-            <v-text-field
-              slot="activator"
-              v-model="date"
-              label="チェックイン日"
-              prepend-icon="event"
-              hint="YYYY/MM/DD format"
-              readonly
-            ></v-text-field>
-            <v-date-picker
-              v-model="date"
-              @input="menu2 = false"
-              locale="jp"
-            ></v-date-picker>
-          </v-menu>
-        </div>
-
-        <div>
-          <v-menu
-            :close-on-content-click="false"
-            v-model="menu2"
-            :nudge-right="40"
-            lazy
-            transition="scale-transition"
-            offset-y
-            full-width
-            min-width="290px"
-          >
-            <v-text-field
-              slot="activator"
-              v-model="date"
-              label="チェックアウト"
-              prepend-icon="event"
-              hint="YYYY/MM/DD format"
-              readonly
-            ></v-text-field>
-            <v-date-picker
-              v-model="date"
-              @input="menu2 = false"
-              locale="jp"
-            ></v-date-picker>
-          </v-menu>
-        </div>
-
-        <div>
-          <v-select
-            :items="items"
-            box
-            label="ご宿泊先"
-          ></v-select>
-        </div>
-
-        <div>
-          <v-select
-            :items="items"
-            box
-            label="お部屋タイプ"
-          ></v-select>
-        </div>
-
-      </div>
-
-    </v-navigation-drawer>
-
     <!-- ツールバー -->
     <v-toolbar
       color="blue-grey"
@@ -114,54 +8,11 @@
       app
       clipped-right
     >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <!-- <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon> -->
       <v-toolbar-title v-text="title" />
-      <v-spacer></v-spacer>
-      <v-toolbar-side-icon @click.stop="drawerRight = !drawerRight"></v-toolbar-side-icon>
+      <!-- <v-spacer></v-spacer>
+      <v-toolbar-side-icon @click.stop="drawerRight = !drawerRight"></v-toolbar-side-icon> -->
     </v-toolbar>
-
-    <!-- 左ナビゲーション -->
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-subheader>テンプレート</v-subheader>
-      <v-list expand="true">
-        <v-list-group
-          v-for="item in items"
-          v-model="item.active"
-          :key="item.title"
-          no-action
-        >
-          <v-list-tile slot="activator">
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-
-          <v-list-tile
-            v-for="subItem in item.items"
-            :key="subItem.title"
-            @click=""
-          >
-            <v-list-tile-content>
-              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
-            </v-list-tile-content>
-
-            <v-list-tile-action>
-              <v-icon>{{ subItem.action }}</v-icon>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list-group>
-      </v-list>
-    </v-navigation-drawer>
-
-    <!-- <v-navigation-drawer
-      v-model="left"
-      temporary
-      fixed
-    ></v-navigation-drawer> -->
 
     <!-- コンテンツ -->
     <v-content>
@@ -181,58 +32,6 @@ export default {
     // right: false,
     // left: false,
     title: 'うなりざきメールテンプレ',
-    items: [
-      {
-        action: 'local_activity',
-        title: '2本目〜',
-        active: true,
-        items: [
-          { title: 'PM～他館' },
-          { title: 'PM～宿泊有' },
-          { title: 'PM～徒歩圏内宿' }
-        ]
-      },
-      {
-        action: 'restaurant',
-        title: '3本目〜',
-        active: true,
-        items: [
-          { title: 'PM～他館' },
-          { title: 'PM～宿泊有' },
-          { title: 'PM～徒歩圏内宿' }
-        ]
-      },
-      {
-        action: 'school',
-        title: 'Education',
-        active: true,
-        items: [{ title: 'List Item' }]
-      },
-      {
-        action: 'directions_run',
-        title: 'Family',
-        active: true,
-        items: [{ title: 'List Item' }]
-      },
-      {
-        action: 'healing',
-        title: 'Health',
-        active: true,
-        items: [{ title: 'List Item' }]
-      },
-      {
-        action: 'content_cut',
-        title: 'Office',
-        active: true,
-        items: [{ title: 'List Item' }]
-      },
-      {
-        action: 'local_offer',
-        title: 'Promotions',
-        active: true,
-        items: [{ title: 'List Item' }]
-      }
-    ],
     valid: false,
     date: new Date().toISOString().substr(0, 10)
     // nameRules: [v => !!v || 'required']
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,146 @@
     justify-center
     align-center
   >
+    <!-- 右ドロワー -->
+    <v-navigation-drawer
+      v-model="drawerRight"
+      fixed
+      right
+      clipped
+      app
+    >
+      <div class="container fluid pb-0">
+        <!-- <v-subheader class="black--text title">入力</v-subheader> -->
+        <p>入力</p>
+        <div>
+          <v-text-field
+            label="担当者名"
+            placeholder=""
+          ></v-text-field>
+        </div>
+
+        <div>
+          <v-text-field
+            label="名前"
+            placeholder=""
+          ></v-text-field>
+        </div>
+
+        <!-- <div>
+          <v-text-field
+            label="チェックイン"
+            placeholder=""
+          ></v-text-field>
+        </div> -->
+        <div>
+          <v-menu
+            :close-on-content-click="false"
+            v-model="menu2"
+            :nudge-right="40"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            min-width="290px"
+          >
+            <v-text-field
+              slot="activator"
+              v-model="date"
+              label="チェックイン日"
+              prepend-icon="event"
+              hint="YYYY/MM/DD format"
+              readonly
+            ></v-text-field>
+            <v-date-picker
+              v-model="date"
+              @input="menu2 = false"
+              locale="jp"
+            ></v-date-picker>
+          </v-menu>
+        </div>
+
+        <div>
+          <v-menu
+            :close-on-content-click="false"
+            v-model="menu2"
+            :nudge-right="40"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            min-width="290px"
+          >
+            <v-text-field
+              slot="activator"
+              v-model="date"
+              label="チェックアウト"
+              prepend-icon="event"
+              hint="YYYY/MM/DD format"
+              readonly
+            ></v-text-field>
+            <v-date-picker
+              v-model="date"
+              @input="menu2 = false"
+              locale="jp"
+            ></v-date-picker>
+          </v-menu>
+        </div>
+
+        <div>
+          <v-select
+            :items="items"
+            box
+            label="ご宿泊先"
+          ></v-select>
+        </div>
+
+        <div>
+          <v-select
+            :items="items"
+            box
+            label="お部屋タイプ"
+          ></v-select>
+        </div>
+
+      </div>
+
+    </v-navigation-drawer>
+    <!-- 左ナビゲーション -->
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-subheader>テンプレート</v-subheader>
+      <v-list expand="true">
+        <v-list-group
+          v-for="item in items"
+          v-model="item.active"
+          :key="item.title"
+          no-action
+        >
+          <v-list-tile slot="activator">
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile
+            v-for="subItem in item.items"
+            :key="subItem.title"
+            @click="key=subItem.key"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-icon>{{ subItem.action }}</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-flex
       xs12
       sm8
@@ -14,7 +154,7 @@
         <vuetify-logo />
       </div> -->
       <v-card>
-        <v-card-title class="headline">2本目〜/PM〜他館</v-card-title>
+        <v-card-title class="headline">{{key}}2本目〜/PM〜他館</v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
         </v-card-text>
@@ -36,9 +176,64 @@ import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
+  data: () => ({
+    items: [
+      {
+        action: 'local_activity',
+        title: '2本目〜',
+        active: true,
+        items: [
+          { title: 'PM～他館', code: '2pm_other_hotel' },
+          { title: 'PM～宿泊有', code: '2pm_my_hotel' },
+          { title: 'PM～徒歩圏内宿', code: '2pm_other_near_hotel' }
+        ]
+      },
+      {
+        action: 'restaurant',
+        title: '3本目〜',
+        active: true,
+        items: [
+          { title: 'PM～他館' },
+          { title: 'PM～宿泊有' },
+          { title: 'PM～徒歩圏内宿' }
+        ]
+      },
+      {
+        action: 'school',
+        title: 'Education',
+        active: true,
+        items: [{ title: 'List Item' }]
+      },
+      {
+        action: 'directions_run',
+        title: 'Family',
+        active: true,
+        items: [{ title: 'List Item' }]
+      },
+      {
+        action: 'healing',
+        title: 'Health',
+        active: true,
+        items: [{ title: 'List Item' }]
+      },
+      {
+        action: 'content_cut',
+        title: 'Office',
+        active: true,
+        items: [{ title: 'List Item' }]
+      },
+      {
+        action: 'local_offer',
+        title: 'Promotions',
+        active: true,
+        items: [{ title: 'List Item' }]
+      }
+    ]
+  }),
   components: {
     Logo,
     VuetifyLogo
-  }
+  },
+  props: ['key']
 }
 </script>
